edbaa634cf2225dfcace744bbcd010be4fe5668a
RyanChen <rockspirite@gmail.com>
2020-12-02 09:38:21 +0800

    Enhance the first message method

--- modules/first-message.js
+++ modules/first-message.js
@@ -8,19 +8,35 @@ const addReactions = (message, reactions) => {
 
 module.exports = async (client, id, text, reactions = []) => {
     const channel = await client.channels.fetch(id)
+    const all_message = [];
+    let last_id;
+    let msg_size = 100;
 
-    channel.messages.fetch().then((messages) => {
-        if (messages.size === 0) {
-            // Send a new message
-            channel.send(text).then((message) => {
-                addReactions(message, reactions)
-            })
-        } else {
-            // Edit the existing message
-            for (const message of messages) {
-                message[1].edit(text)
-                addReactions(message[1], reactions)
-            }
+    // Fetch all messages
+    while (msg_size >= 50) {
+        const options = { limit: 50 };
+        if (last_id) {
+            options.before = last_id;
         }
-    })
+
+        await channel.messages.fetch(options).then(messages => {
+            msg_size = messages.size;
+            if (messages.size > 0) {
+                messages.forEach(msg => { all_message.push(msg) })
+                last_id = messages.last().id;
+            }
+        });
+    }
+
+    if (all_message.length === 0) {
+        // Send a new message
+        channel.send(text).then((message) => {
+            addReactions(message, reactions)
+        })
+    } else {
+        // Edit the existing message
+        first_message = all_message.pop();
+        first_message.edit(text)
+        addReactions(first_message, reactions)
+    }
 }
