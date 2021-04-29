55771fcad95dae119396393f3dc331a02b758da3
RyanChen <rockspirite@gmail.com>
2021-04-29 15:23:21 +0800

    Serial number function introduced

--- configs/message.js
+++ configs/message.js
@@ -3,7 +3,7 @@ const {prefix, version} = require('./config.json');
 const commands = require('./commands.json');
 
 const BotInfoMsgEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
+	.setColor('#ff0000')
 	.setTitle('MapleBot Information')
 	// .setURL('https://discord.js.org/')
 	.setAuthor('RyanChen', 'https://i.imgur.com/GMIaI2q.png')
@@ -18,7 +18,7 @@ const BotInfoMsgEmbed = new Discord.MessageEmbed()
     .setFooter('Copyright © 2020 RyanChen All rights reserved.', 'https://i.imgur.com/GMIaI2q.png');
     
 const ManagersMsgEmbed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
+    .setColor('#ff00ff')
     .setTitle('管理者資訊')
     .setDescription('公會會長以及副會長資訊列表')
     .setThumbnail('https://i.imgur.com/irYgCg7.png')
@@ -26,6 +26,34 @@ const ManagersMsgEmbed = new Discord.MessageEmbed()
 		{ name: '會長', value: '綠綠安m' },
         { name: '副會長', value: 'Ryan迪迪\n折花無痕\n水裡的魚小冷\nHeyYeah黑夜c\n薏仁0e\nFF鼻樑終結者\n急凍小美'},
     )
+
+let set_list_serial_msg = (serial_data) => {
+    return new Promise((resolve, reject) => {
+        ListSerialMsgEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .addFields(
+            { name: 'Serial Information', value: serial_data, inline: true }
+        )
+        .setTimestamp()
+        .setFooter('Copyright © 2021 RyanChen All rights reserved.', 'https://i.imgur.com/GMIaI2q.png');
+        resolve(ListSerialMsgEmbed)
+    }).catch(error => { console.log(error); reject(false); });
+};
+
+let set_single_serial_msg = (serial_data) => {
+    return new Promise((resolve, reject) => {
+        SingleSerialMsgEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .addFields(
+            { name: '名稱', value: serial_data.rows[0].name, inline: true },
+            { name: '序號', value: serial_data.rows[0].serial_number, inline: true },
+            { name: '使用期限', value: serial_data.rows[0].due_date == null ? "-" : serial_data.rows[0].due_date, inline: true },
+        )
+        .setTimestamp()
+        .setFooter('Copyright © 2021 RyanChen All rights reserved.', 'https://i.imgur.com/GMIaI2q.png');
+        resolve(SingleSerialMsgEmbed)
+    }).catch(error => { console.log(error); reject(false); });
+};
 
 try {
     command = new Array()
@@ -38,7 +66,7 @@ try {
 }
 
 const CommandsMsgEmbed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
+    .setColor('#00ff00')
     .setTitle('Commands')
     .setDescription('All commands of this bot are list below:')
     .addFields(command)
@@ -52,4 +80,12 @@ module.exports =  {
     BotInfoMsgEmbed: BotInfoMsgEmbed,
     ManagersMsgEmbed: ManagersMsgEmbed,
     CommandsMsgEmbed: CommandsMsgEmbed
+};
+
+module.exports.set_list_serial_msg = function (serial_data) {
+    return set_list_serial_msg(serial_data)
+};
+
+module.exports.set_single_serial_msg = function (serial_data) {
+    return set_single_serial_msg(serial_data)
 };
---
+++ modules/serial_number.js
@@ -0,0 +1,230 @@
+var pg = require('pg');
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports.create_table = function (sql) {
+    return create_table(sql)
+};
+
+module.exports.drop_table = function (table_name) {
+    return rop_table(table_name)
+};
+
+module.exports.insert_serial = function (serial_data, sender) {
+    return insert_serial(serial_data, sender)
+};
+
+module.exports.get_serial = function (id, recipient) {
+    return get_serial(id, recipient)
+};
+
+module.exports.list_serial = function () {
+    return list_serial()
+};
+
+// Create table
+let create_table = (sql) => {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, (err, res) => {
+            if (err) {
+                console.log(err, res)
+                reject(false)
+            }
+            else {
+                resolve(true)
+            }
+        })
+    }).catch(error => { console.log(error); reject(false); });
+};
+
+// Drop table
+let drop_table = (table_name) => {
+    return new Promise((resolve, reject) => {
+        console.log("drop table")
+        sql = 'DROP TABLE IF EXISTS ' + table_name + ';'
+        pool.query(sql, (err, res) => {
+            if (err) {
+                console.log(err, res)
+                reject(false)
+            }
+            else {
+                resolve(res)
+            }
+        })
+    }).catch(error => { console.log(error); return ; });
+};
+
+// Insert serial data to table
+let insert_serial = (serial_data, sender) => {
+    return new Promise((resolve, reject) => {
+        //Check row numbers
+        check_row_count('serial').then((res) => {
+            if (res >= 10000)
+            {
+                delete_first_row('serial')
+            }
+        }).then(() => {
+            console.log("===== Serial Info =====")
+            parameters = serial_data.split('^');
+            name = parameters[0]
+            serial_number = parameters[1]
+            if (parameters[2] == undefined || parameters[2] == "")
+            {
+                due_date = "NULL"
+            }
+            else
+            {
+                due_date = "TO_TIMESTAMP('" + parameters[2] + "', 'YYYY-MM-DD HH:MI:SS')"
+            }
+            console.log("Name = " + name)
+            console.log("serial_number = " + serial_number)
+            console.log("due_date = " + due_date)
+            console.log("share_by = " + sender)
+            console.log("===== Serial Info =====")
+
+            sql = `INSERT INTO serial (name, serial_number, due_date, available, share_time, share_by, recipient, send_time) VALUES ('${name}', '${serial_number}', ${due_date}, true, NOW(), '${sender}', NULL, NULL)`
+
+            pool.query(sql, (err, res) => {
+                if (err) {
+                    console.log(err, res)
+                    reject(false)
+                }
+                else {
+                    console.log(res)
+                    resolve(true)
+                }
+            })
+        })
+    }).catch(error => { console.log(error); reject(false); });
+};
+
+// Delete data from table
+let get_serial = (id, recipient) => {
+    return new Promise((resolve, reject) => {
+        get_serial_sql = `SELECT id, name, serial_number, COALESCE(TO_CHAR(due_date, 'YYYY-MM-DD HH:MI:SS'), NULL) due_date, available, COALESCE(TO_CHAR(share_time, 'YYYY-MM-DD HH:MI:SS'), 'NULL') share_time, share_by, recipient, send_time FROM serial where id = ${id}`
+
+        pool.query(get_serial_sql, (err, get_res) => {
+            if (err) {
+                console.log(err, get_res)
+                resolve(false)
+            }
+            else {
+                console.log(get_res)
+
+                if (get_res.rowCount <= 0) {
+                    resolve(false)
+                }
+                else {
+                    //Check row numbers
+                    check_row_count('used_serial').then((res) => {
+                        if (res >= 10000)
+                        {
+                            delete_first_row('used_serial')
+                        }
+                    }).then(() => {
+                        id = get_res.rows[0].id
+                        name = "'" + get_res.rows[0].name + "'"
+                        serial_number = "'" + get_res.rows[0].serial_number + "'"
+
+                        if (get_res.rows[0].due_date == null) {
+                            due_date = "NULL"
+                        }
+                        else {
+                            due_date = "TO_TIMESTAMP('" + get_res.rows[0].due_date + "', 'YYYY-MM-DD HH:MI:SS')" 
+                        }
+                        available = get_res.rows[0].available
+                        share_time = "TO_TIMESTAMP('" + get_res.rows[0].share_time + "', 'YYYY-MM-DD HH:MI:SS')" 
+                        share_by = "'" + get_res.rows[0].share_by + "'"
+
+                        insert_serial_sql = `INSERT INTO used_serial (serial_id, name, serial_number, due_date, available, share_time, share_by, recipient, send_time) VALUES (${id}, ${name}, ${serial_number}, ${due_date}, ${available}, ${share_time}, ${share_by}, ${recipient}, NOW())`
+                        console.log(insert_serial_sql)
+
+                        pool.query(insert_serial_sql, (err, res) => {
+                            if (err) {
+                                console.log(err, res)
+                                resolve(false)
+                            }
+                            else {
+                                delete_serial_sql = `DELETE FROM serial WHERE id = ${id}`
+
+                                pool.query(delete_serial_sql, (err, res) => {
+                                    if (err) {
+                                        console.log(err, res)
+                                        resolve(false)
+                                    }
+                                    else {
+                                        delete_serial_sql = `DELETE FROM serial WHERE id = ${id}`
+                                        console.log(`Serial was deleted, id = ${id}`)
+                                        resolve(get_res)
+                                    }
+                                })
+                            }
+                        })
+                    })
+                }
+            }
+        })
+    }).catch(error => { console.log(error); reject(false); });
+};
+
+// Get all available serial from table
+let list_serial = () => {
+    return new Promise((resolve, reject) => {
+        sql = `SELECT id, name, serial_number, COALESCE(TO_CHAR(due_date, 'YYYY-MM-DD HH:MI:SS'), '') due_date, available, share_time, share_by, recipient, send_time FROM serial where available is not false order by id`
+
+        pool.query(sql, (err, res) => {
+            if (err) {
+                console.log(err, res)
+                reject(false)
+            }
+            else {
+                if (res.rowCount <= 0) {
+                    resolve(false)
+                }
+                else {
+                    resolve(res)
+                }
+            }
+        })
+    }).catch(error => { console.log(error); reject(false); });
+};
+
+// Check row count
+let check_row_count = (table_name) => {
+    return new Promise((resolve, reject) => {
+        check_row_count_sql = `SELECT COUNT(*) count FROM ${table_name}`
+        pool.query(check_row_count_sql, (err, check_res) => {
+            if (err) {
+                console.log(err, check_res)
+                resolve(0)
+            }
+            else {
+                row_count = check_res.rows[0].count
+                console.log(`${table_name} row count = ${row_count}`)
+                resolve(row_count)
+            }
+        })
+    }).catch(error => { console.log(error); reject(0); });
+};
+
+// Delete first row data count
+let delete_first_row = (table_name) => {
+    return new Promise((resolve, reject) => {
+        delete_first_row_sql = `DELETE FROM ${table_name} WHERE id = (SELECT min(id) FROM  ${table_name})`
+        pool.query(delete_first_row_sql, (err, check_res) => {
+            if (err) {
+                console.log(err, check_res)
+                resolve(false)
+            }
+            else {
+                console.log(`Data over limit, delete first data`)
+                resolve(true)
+            }
+        })
+    }).catch(error => { console.log(error); reject(0); });
+};
